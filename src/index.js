e3eb60d26e2f0b5b5010ed1e6d95fd5e7ec0200a
Aksultan <aksultan919@gmail.com>
2020-10-11 18:25:30 +0600

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = [].concat.apply([],bracketsConfig)
+  return str.split("").reduce((x, y)=>((x[x.length-1]==bracketsConfig[bracketsConfig.lastIndexOf(y)-1]&&bracketsConfig.lastIndexOf(y)%2!=0)?x.pop():x.push(y),x),[]).length>0?false:true
 }

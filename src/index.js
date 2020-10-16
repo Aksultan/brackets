2bf5fad0483cec99c56903b59565884c998c6d82
Aksultan <aksultan919@gmail.com>
2020-10-16 23:11:30 +0600

    refactor: make it a bit readable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function check(str, bracketsConfig) {
-  bracketsConfig = [].concat.apply([],bracketsConfig)
-  return str.split("").reduce((x, y)=>((x[x.length-1]==bracketsConfig[bracketsConfig.lastIndexOf(y)-1]&&bracketsConfig.lastIndexOf(y)%2!=0)?x.pop():x.push(y),x),[]).length>0?false:true
+  let brack = [].concat.apply([],bracketsConfig)
+  return str.split("").reduce((x, y)=>(( x[x.length-1]==brack[brack.lastIndexOf(y)-1] && brack.lastIndexOf(y)%2 )?x.pop():x.push(y),x),[]).length==0
 }
